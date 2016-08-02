b44fa4d7bb4d39bcfc952add633b05b246bdc8df
Mustafa ÖZDEMİR <mustafa.ozdemir@pirimedya.com>
2016-08-02 14:07:26 +0000

    Updated ContentTypeConstant.ts

--- ApiSimulation/Scripts/Commons/Constants/ContentTypeConstant.ts
+++ ApiSimulation/Scripts/Commons/Constants/ContentTypeConstant.ts
@@ -2,6 +2,6 @@
 {
     export class ContentTypeConstant
     {
-        public static AllowedContentTypes: Array<string> = ["application/json", "application/xml", "text/html", "text/plain", "text/csv", "image/png", "image/jpeg", "application/pdf"];
+        public static AllowedContentTypes: Array<string> = ["application/json", "application/xml", "text/html", "text/plain", "text/csv", "image/png", "image/jpeg", "application/pdf", "video/mp4"];
     }      
 }
