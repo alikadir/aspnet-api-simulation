dd9233ae56b8b5fbb9f6d85d06a27df7dd49deb4
Ali Kadir BAĞCIOĞLU <Ali Kadir BACIOLU>
2016-08-22 12:27:09 +0300

    Response.TrySkipIisCustomErrors eklendi

--- ApiSimulation/Scripts/Commons/Constants/ContentTypeConstant.ts
+++ ApiSimulation/Scripts/Commons/Constants/ContentTypeConstant.ts
@@ -3,5 +3,5 @@
     export class ContentTypeConstant
     {
         public static AllowedContentTypes: Array<string> = ["application/json", "application/xml", "text/html", "text/plain", "text/csv", "image/png", "image/jpeg", "application/pdf", "video/mp4"];
-    }      
+    }
 }
--- ApiSimulation/gruntfile.js
+++ ApiSimulation/gruntfile.js
@@ -1,10 +1,7 @@
 ﻿/// <reference path="C:\ApiSimulation\ApiSimulation\Scripts/Lib/jquery-2.2.2.js" />
 /// <reference path="C:\ApiSimulation\ApiSimulation\Scripts/Lib/jquery-2.2.2.js" />
 /// <binding BeforeBuild='concat' ProjectOpened='watch' />
-/*
-This file in the main entry point for defining grunt tasks and using grunt plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
-*/
+
 module.exports = function (grunt)
 {
     grunt.initConfig({
@@ -18,7 +15,7 @@ module.exports = function (grunt)
             },
 
             internalScript: { // high priority file *Base.js  =>  src: ['lib/before.js', 'lib/*', 'lib/after.js'] 
-                src: ['Scripts/Commons/**/*.js','Scripts/Models/**/*Base.js', 'Scripts/Models/**/*.js', 'Scripts/Views/*.js'],
+                src: ['Scripts/Commons/**/*.js', 'Scripts/Models/**/*Base.js', 'Scripts/Models/**/*.js', 'Scripts/Views/*.js'],
                 dest: 'Scripts/Dist/internal.js',
             },
 
@@ -33,6 +30,7 @@ module.exports = function (grunt)
             },
 
         },
+
         /*        
         uglify: {
             scripts: {
@@ -51,14 +49,16 @@ module.exports = function (grunt)
         watch: { // ignore için "!" kullanılır
             scripts: {
                 files: ['Scripts/**/*.js'],// '!Scripts/Dist/*'],
-                tasks: ['concat', 'uglify'],
+                // tasks: ['concat', 'uglify'],
+                tasks: ['concat'],
                 options: {
                     spawn: false,
                 },
             },
             styles: {
                 files: ['Styles/**/*.css'],// '!Styles/Dist/*'],
-                tasks: ['concat', 'uglify'],
+                // tasks: ['concat', 'uglify'],
+                tasks: ['concat'],
                 options: {
                     spawn: false,
                 },
@@ -69,5 +69,5 @@ module.exports = function (grunt)
 
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
+  //  grunt.loadNpmTasks("grunt-contrib-uglify");
 };
